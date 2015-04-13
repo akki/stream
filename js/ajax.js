c60d3bf996ab3309a8c1aa588f497b9cbb155eb1
Akshesh <aksheshdoshi@gmail.com>
2015-04-14 00:45:19 +0530

    Change request link in ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,33 +1,35 @@
-var server = "172.25.55.156";
+
+// var server = "stream.channeli.in";
 
 openWin("https://youtube.com");
 closeWin();
 function openWin(site)
 {
 myWindow=window.open("song.php?site=" + site); //,"_blank",'');
-// myWindow.blur();
+myWindow.blur();
 myWindow.focus();
 }
 
 function closeWin()
 {
-myWindow.close();
+  myWindow.close();
 }
 
 function refresh()
 {
   $.ajax({
-    url: "/stream/player.php",
+    url: "./player.php",
     datatype: 'html',
     success: function(data)
     {
       if(data!="")
       {
         closeWin();
-        openWin(data);
+         openWin(data);
         data="";
       }
       setTimeout(refresh, 4000); // you could choose not to continue on failure...
     }
   });
 }
+
